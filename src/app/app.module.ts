07dc8ba2092f65a752dafc6711b8a05cc9f1e00f
Trina Castillo <trina.castillo@expeditors.com>
2017-07-26 16:47:20 -0700

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ViewListComponent } from './view-list/view-list.component';
+import { AddItemComponent } from './add-item/add-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ViewListComponent,
+        AddItemComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            {
+                path: '',
+                redirectTo: 'list',
+                pathMatch: 'full'
+            },
+            {
+                path: 'list',
+                component: ViewListComponent
+            },
+            {
+                path: 'item/new',
+                component: AddItemComponent
+            }
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
